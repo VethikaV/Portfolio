e6a5581a2412fdc00fc3cd1feaeb09ff3e214542
Vethika <vethika1681@gmail.com>
2025-02-23 22:27:03 +0530

    Done the portfolio

---
+++ portfolio/src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import './css/navbar.css';
+
+const Nav = () => {
+  return (
+    <header className='navbar'>
+      <nav>
+        <ul>
+          <li><Link className="li" to="/home">Home</Link></li>
+          <li><Link className="li" to="/about">About</Link></li>
+          <li><Link className="li" to="/project">Projects</Link></li>
+          <li><Link className="li" to="/achievement">Achievement</Link></li>
+          <li><Link className="li" to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
